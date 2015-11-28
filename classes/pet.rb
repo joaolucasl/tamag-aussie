76da2f3d3c1021b800627de53ae78a0c9b53163a
Byron Mejia <byron.mejia@connect.qut.edu.au>
2015-11-28 17:54:51 +1000

    Added home and current to class

--- classes/pet.rb
+++ classes/pet.rb
@@ -1,13 +1,14 @@
 class Pet
 
-  def initialize(name)
+  def initialize(name, home, current)
     @name = name
     @health = 100
     @intelligence = 100
     @happiness = 100
     @hunger = 100
     @coins = 100
-    @home
-    @current
+    @home = home
+    @current = current
   end
+
 end
