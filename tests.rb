a598b18f522b01984f6053728e1659f4a713bf39
Byron Mejia <byron.mejia@connect.qut.edu.au>
2015-11-28 21:21:29 +1000

    Rewrote manual tests for new data type

--- tests.rb
+++ tests.rb
@@ -6,11 +6,11 @@ puts "\nLocation test:"
 here = Location.new(-27.4667, 153.0333)
 
 puts "Lat:  #{here.lat}"
-puts "Long: #{here.long}"
+puts "Long: #{here.lng}"
 
 puts "\nPet test:"
 
 pet = Pet.new('Bobby', here, here)
 puts "Name: #{pet.name}"
 puts "Home: #{pet.home.lat}"
-puts "    : #{pet.home.long}"
+puts "    : #{pet.home.lng}"
