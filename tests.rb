30471cd4a94c92d465ecbf1b296459aba01d6b71
Byron Mejia <byron.mejia@connect.qut.edu.au>
2015-11-28 18:30:24 +1000

    Manual Test to check assessors

--- tests.rb
+++ tests.rb
@@ -1,1 +1,16 @@
 require_relative 'classes/location'
+require_relative 'classes/pet'
+
+puts "\nLocation test:"
+
+here = Location.new(-27.4667, 153.0333)
+
+puts "Lat:  #{here.lat}"
+puts "Long: #{here.long}"
+
+puts "\nPet test:"
+
+pet = Pet.new('Bobby', here, here)
+puts "Name: #{pet.name}"
+puts "Home: #{pet.home.lat}"
+puts "    : #{pet.home.long}"
