2e7a6ff0e837d53545f0b924938d9d957aae4eb6
Byron Mejia <byron.mejia@connect.qut.edu.au>
2015-11-28 21:23:25 +1000

    Added JSON parser to remove unnecessary typing.

--- helpers/helpers.rb
+++ helpers/helpers.rb
@@ -1,7 +1,20 @@
+require 'json'
+
 def internet?
   begin
     true if open('http://www.google.com')
   rescue
     false
   end
+end
+
+def get_key(type)
+  return JSON.parse(
+            File.read(
+              File.join(
+                File.dirname(File.dirname(File.absolute_path(__FILE__))),
+                'config/keys.json'
+              )
+            )
+          )['key'][type]
 end
