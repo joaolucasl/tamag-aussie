b781b8e5d2c18a129b4813cabd0d581d2838ffdb
Byron Mejia <byron.mejia@connect.qut.edu.au>
2015-11-28 18:32:03 +1000

    Converted accessors to shorthand

--- classes/location.rb
+++ classes/location.rb
@@ -1,23 +1,10 @@
 class Location
+  attr_accessor :lat
+  attr_accessor :long
+
   def initialize(lat, long)
     @lat = lat
     @long = long
-  end
-
-  def lat
-    @lat
-  end
-
-  def lat=(new_lat)
-    @lat = new_lat
-  end
-
-  def long
-    @long
-  end
-
-  def lat=(new_long)
-    @long = new_long
   end
 
 end
